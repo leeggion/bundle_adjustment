6d1373d1da5b37420f850b36feabc7b941b10935
YONKanzer <144446744+YONKanzer@users.noreply.github.com>
2025-10-29 22:23:04 +0300

    Update math.hpp

--- src/utilities/math.hpp
+++ src/utilities/math.hpp
@@ -6,19 +6,6 @@
 #include "Structs.hpp"
 
 // shit
-Eigen::Vector2d projection(const Camera& cam, const Point3D& p) {
-    Eigen::AngleAxisd aa(cam.rotation.norm(), cam.rotation.normalized());
-    Eigen::Vector3d Pc = aa.toRotationMatrix() * p.pos + cam.translation;
-
-    double x = Pc[0] / Pc[2];
-    double y = Pc[1] / Pc[2];
-    double r2 = x * x + y * y;
-    double radial = 1.0 + cam.k1 * r2 + cam.k2 * r2 * r2;
-
-    double u = cam.f * radial * x;
-    double v = cam.f * radial * y;
-
-    return {u, v};
-}
+Eigen::Vector2d projection(const Camera& cam, const Point3D& p);
 
 #endif /* _t33nsy_MATH */
